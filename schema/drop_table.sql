6cbb4af7465d493187628ce01baa6d65776026c6
xodivorce <prasidmandal79@gmail.com>
2026-01-30 11:31:06 +0530

    (refactor): improve project structure and add drop-table schema

---
+++ schema/drop_table.sql
@@ -0,0 +1,16 @@
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS
+  `blogs`,
+  `career_applications`,
+  `certificates`,
+  `contact_submissions`,
+  `courses`,
+  `events`,
+  `job_openings`,
+  `students`,
+  `testimonials`,
+  `users`;
+
+
+SET FOREIGN_KEY_CHECKS = 1;
